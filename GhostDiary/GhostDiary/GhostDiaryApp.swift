6948abb64fbb510697862c29c9326d966d32fc36
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2022-12-14 20:46:14 +0900

    init project setting
    init project setting

---
+++ GhostDiary/GhostDiary/GhostDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostDiaryApp.swift
+//  GhostDiary
+//
+//  Created by 이학진 on 2022/12/14.
+//
+
+import SwiftUI
+
+@main
+struct GhostDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
