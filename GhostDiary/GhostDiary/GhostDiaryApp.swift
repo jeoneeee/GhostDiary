30384215917044cd35841fca66f016004cd67eae
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2022-12-17 23:32:56 +0900

    [Feat] 로그인 뷰 구현
    [Feat] 로그인 뷰 구현

--- GhostDiary/GhostDiary/GhostDiaryApp.swift
+++ GhostDiary/GhostDiary/GhostDiaryApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GhostDiaryApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
--- GhostDiary/GhostDiary/View/Login/LoginView.swift
+++ GhostDiary/GhostDiary/View/Login/LoginView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var isSingUp: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("이메일을 입력하세요. ", text: $email)
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        .stroke(Color(UIColor.systemGray4), lineWidth: 2)
+                }
+                .padding()
+            //FIXME: - TextField -> SecureField로 수정 예정
+            TextField("비밀번호를 입력하세요. ", text: $password)
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        .stroke(Color(UIColor.systemGray4), lineWidth: 2)
+                }
+                .padding()
+            //FIXME: - Color 수정 예정
+            Button(action: {
+                isSingUp.toggle()
+            }, label: {
+                Text("Sign In")
+                    .padding()
+            })
+            .frame(maxWidth: UIScreen.main.bounds.width)
+            .foregroundColor(.white)
+                .background(Color.red)
+                .cornerRadius(40)
+        }
+        .padding()
+        .fullScreenCover(isPresented: $isSingUp) {
+            SignUpView()
+        }
     }
 }
 
