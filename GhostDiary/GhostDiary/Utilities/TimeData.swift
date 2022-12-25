ee1924551d837f985b776d4ab2649bb0176f5713
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2022-12-25 13:38:13 +0900

    [Feat] getTimeStrings 메소드 기본값 파라미터 추가

--- GhostDiary/GhostDiary/Utilities/TimeData.swift
+++ GhostDiary/GhostDiary/Utilities/TimeData.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 struct TimeData {
-    static func getTimeStrings() -> String{
+    static func getTimeStrings(_ date: Date = Date(timeIntervalSince1970: Date().timeIntervalSince1970)) -> String{
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ko_kr")
         dateFormatter.timeZone = TimeZone(abbreviation: "KST")
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm" // "yyyy-MM-dd HH:mm:ss"
         
-        let dateCreatedAt = Date(timeIntervalSince1970: Date().timeIntervalSince1970)
+        let dateCreatedAt = date
         let date = dateFormatter.string(from: dateCreatedAt)
         
         return date
