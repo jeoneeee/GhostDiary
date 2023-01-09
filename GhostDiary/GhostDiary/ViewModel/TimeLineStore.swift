530f09d8eca467289e8c5011ecf104c8789a9ba5
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-09 22:19:08 +0900

    [Add] TimeLineStore 파일 추가

---
+++ GhostDiary/GhostDiary/ViewModel/TimeLineStore.swift
@@ -0,0 +1,13 @@
+//
+//  TimeLineStore.swift
+//  GhostDiary
+//
+//  Created by 이학진 on 2023/01/09.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class TimeLineStore {
+    @Published var answers: [Answer] = []
+}
