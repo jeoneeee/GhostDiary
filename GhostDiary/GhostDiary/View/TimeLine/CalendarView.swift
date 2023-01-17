df0f5ea201eab5371524710f167891b1b8859111
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-17 20:38:38 +0900

    [Feat] 유저가 대답한 질문을 가져오는 기능 구현

--- GhostDiary/GhostDiary/View/TimeLine/CalendarView.swift
+++ GhostDiary/GhostDiary/View/TimeLine/CalendarView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct CalendarView: View {
     @State private var currentDate: Date = Date()
     @EnvironmentObject var authStores: AuthStore
+    @EnvironmentObject var answerStores: AnswerStore
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             CustomDatePicker(currentDate: $currentDate)
         }
         .padding(.horizontal, 20)
+        
+        .onAppear {
+            Task {
+                if let user = authStores.user {
+                    await answerStores.readAnswer(user)
+                }
+            }
+        }
     }
 }
 
@@ -23,5 +32,6 @@ struct CalendarView_Previews: PreviewProvider {
     static var previews: some View {
         CalendarView()
             .environmentObject(AuthStore())
+            .environmentObject(AnswerStore())
     }
 }
--- GhostDiary/GhostDiary/ViewModel/AnswerStore.swift
+++ GhostDiary/GhostDiary/ViewModel/AnswerStore.swift
@@ -9,15 +9,16 @@ import Foundation
 import FirebaseFirestore
 
 class AnswerStore: ObservableObject {
+    @Published var questions: [Question] = [] // 내가 대답한 질문들
+    @Published var answers: [Answer] = [] // 내가 대답한 답변들
     
-    let database = Firestore.firestore()
     
+    let database = Firestore.firestore().collection("Questions")
     
     //사용자가 작성한 답변 추가
     func createAnswer(_ questionId: String, answer: Answer) async {
-
         do {
-            try await database.collection("Questions")
+            try await database
                 .document(questionId).collection("Answers")
                 .document(answer.id)
                 .setData(["id": answer.id,
@@ -28,7 +29,26 @@ class AnswerStore: ObservableObject {
         } catch {
             print("creatAnswers error : \(error.localizedDescription)")
         }
-        
+    }
+    
+    
+    // MARK: - 사용자가 작성한 답변을 가져오는 메소드
+    /// - Parameter user: 로그인한 사용자의 user정보를 가지고 있습니다
+    ///
+    /// 사용자가 작성한 답변을 가져오는 메소드입니다
+    func readAnswer(_ user: User) async {
+        do {
+            let questionSnapshot = try await database
+                .whereField("number", isLessThanOrEqualTo: user.questionNum)
+                .getDocuments()
+            
+            for document in questionSnapshot.documents {
+                print("document.documentID: \(document.documentID)")
+            }
+            
+        } catch {
+            print("qestion get error: \(error.localizedDescription)")
+        }
     }
 }
 
