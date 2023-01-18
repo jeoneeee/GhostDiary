339bc20f957a630b7abdd2d3f1e6685c8960cf7d
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-18 21:00:18 +0900

    [Chore] Calendar에서 필요한 주석 추가 및 주석 제거

--- GhostDiary/GhostDiary/Ex+Date.swift
+++ GhostDiary/GhostDiary/Ex+Date.swift
@@ -7,16 +7,17 @@
 
 import Foundation
 
-// Extending Date to get Current Month Dates
 extension Date {
+    // MARK: - 현재 호출하는 Date값의 달의 날짜 정보들을 출력하는 메소드
+    /// 현재 2023년 4월의 값을 가진 date가 메소드를 호출했다면
+    /// 4월의 date값들을 배열에 담아 반환한다.
+    /// ex) [2023-02-28 15:00:00 +0000, 2023-03-01 15:00:00 +0000]
     func getAllDates() -> [Date] {
         let calendar = Calendar.current
         
-        // getting start Date
         let startDate = calendar.date(from: Calendar.current.dateComponents([.year, .month], from: self))!
         let range = calendar.range(of: .day, in: .month, for: startDate)!
         
-        // getting date
         return range.compactMap { day -> Date in
             return calendar.date(byAdding: .day, value: day - 1, to: startDate)!
         }
--- GhostDiary/GhostDiary/View/TimeLine/CustomDatePicker.swift
+++ GhostDiary/GhostDiary/View/TimeLine/CustomDatePicker.swift
@@ -133,48 +133,44 @@ struct CustomDatePicker: View {
         }
     }
     
-    // extracting year and month for display
+    // MARK: - currentDate을 기준으로 년도와 일을 문자열 배열로 반환하는 메소드
+    /// 반환값: ex) [2023, 4월]
     func extraData() -> [String] {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko")
         formatter.dateFormat = "YYYY MMMM"
         
         let date = formatter.string(from: currentDate)
-        
-        print("ss : \(date.components(separatedBy: " "))")
-        
         return date.components(separatedBy: " ")
     }
-    
+    // MARK: - 월을 변경하는 버튼을 눌렀을때 호출되는 메소드로 현재 화면에 보이는 월을 반환한다.
+    /// 반환값: 현재 2023년 4월을 보고있다면 currentMonth: 2023-04-18 11:46:36 +0000
     func getCurrentMonth() -> Date {
         let calendar = Calendar.current
         
         guard let currentMonth = calendar.date(byAdding: .month, value: self.currentMonth, to: Date()) else { return Date() }
-        
         return currentMonth
     }
     
+    // MARK: - 일 정보를 반환하는 메소드
+    /// 반환값은 CalendarDate 타입의 배열이다.
+    /// 현재 화면에 보고 있는 달력의 날짜들을 CalendarDate 타입으로 반환한다.
+    /// 달력의 첫줄에 비어있는 칸은 day 프로퍼티의 값이 -1이다.
     func extractDate() -> [CalendarDate] {
-        // Getting Current Month Date
-        
         let calendar = Calendar.current
-        print("calendar: \(calendar)")
-        
         let currentMonth = getCurrentMonth()
         
+        // 현재 달력의 Date타입의 값들을 custom model인 CalendarDate로 변환하는 작업
         var days =  currentMonth.getAllDates().compactMap { date -> CalendarDate in
             let day = calendar.component(.day, from: date)
-            
             return CalendarDate(day: day, date: date)
         }
-
-        // adding offeset days
-        let firstWeekday = calendar.component(.weekday, from: days.first?.date ?? Date())
         
+        // 현재 월의 첫날이 달력의 시작부터 얼마나 떨어져있는지 계산하는 로직
+        let firstWeekday = calendar.component(.weekday, from: days.first?.date ?? Date())
         for _ in 0..<firstWeekday - 1 {
             days.insert(CalendarDate(day: -1, date: Date()), at: 0)
         }
-        
         return days
     }
 }
