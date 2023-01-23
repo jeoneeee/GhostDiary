261227bc1febdc72a3a1fc5c059023a088f2df49
jeoneeee <wldus677@gmail.com>
2023-01-23 17:51:02 +0900

    [Feat] ChartView 이모지에 따른 갯수 측정

--- GhostDiary/GhostDiary/View/Analysis/ChartView.swift
+++ GhostDiary/GhostDiary/View/Analysis/ChartView.swift
@@ -11,16 +11,40 @@ import Charts
 struct ChartView: View {
     let emoji = ["angry", "cute", "proud", "sad", "tired", "umm"]
     let text = ["1", "2", "3", "4", "5", "6"]
-    let steps = [1,2,3,4,5,6]
+    @State var steps = Array(repeating: 0, count: 6)
     @EnvironmentObject var answerStore: AnswerStore
     @EnvironmentObject var authStore: AuthStore
+    var angryCount: Int {
+        let angryArray = answerStore.answers.filter{$0.expression == "angry"}
+        return angryArray.count
+    }
+    var cuteCount: Int {
+        let cuteArray = answerStore.answers.filter{$0.expression == "cute"}
+        return cuteArray.count
+    }
+    var proudCount: Int {
+        let proudArray = answerStore.answers.filter{$0.expression == "proud"}
+        return proudArray.count
+    }
+    var sadCount: Int {
+        let sadArray = answerStore.answers.filter{$0.expression == "sad"}
+        return sadArray.count
+    }
+    var tiredCount: Int {
+        let tiredArray = answerStore.answers.filter{$0.expression == "tired"}
+        return tiredArray.count
+    }
+    var ummCount: Int {
+        let ummArray = answerStore.answers.filter{$0.expression == "umm"}
+        return ummArray.count
+    }
     
     var body: some View {
             VStack {
                 Chart {
                     ForEach(emoji.indices, id: \.self) { index in
                         BarMark(x: .value("Emoji", text[index]), y: .value("Steps", steps[index]))
-                            .foregroundStyle(by: .value("Day", text[index]))
+                            .foregroundStyle(by: .value("Texts", text[index]))
                             .annotation(position: .top) {
                                 VStack {
                                     Text("\(steps[index])")
@@ -36,13 +60,10 @@ struct ChartView: View {
                 .chartForegroundStyleScale([
                     "1": Color("Color8"), "2": Color("Color3"), "3": Color("Color4"), "4": Color("Color1"), "5": Color("Color6"), "6": Color("Color7")
                 ])
-                .frame(height: 400)
-                
-//                ForEach(answerStore.answers) { idx in
-//                    Text("expression = \(idx.expression.count)")
-//                }
-                Text("Gd")
-                
+                .frame(height: 400)                
+            }
+            .onAppear {
+                steps = [angryCount,cuteCount,proudCount,sadCount,tiredCount,ummCount]
             }
     }
 }
