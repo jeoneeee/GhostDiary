440f6abb7e868333472324b23cff2be8843c20d6
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-25 18:05:43 +0900

    [Style] 이메일로 로그인하는 Component들 이동

--- GhostDiary/GhostDiary/View/Login/LoginView.swift
+++ GhostDiary/GhostDiary/View/Login/LoginView.swift
@@ -12,6 +12,7 @@ import AuthenticationServices
 
 struct LoginView: View {
     @EnvironmentObject var authStores: AuthStore
+    @Environment(\.colorScheme) var colorScheme
     
     @Environment(\.window) var window: UIWindow?
     @State private var appleLoginCoordinator: AppleAuthCoordinator?
@@ -19,83 +20,12 @@ struct LoginView: View {
     @Binding var isLogin: Bool
     @Binding var isLoading: Bool
     
-    @State var email: String = ""
-    @State var password: String = ""
     @State var isSingUp: Bool = false
+    @State private var isEmailLogin: Bool = false
     
-    @State var isPasswordHidden: Bool = false
-    @State var loginMessage: String = ""
     
-    var body: some View {
+    var socialLoginButton: some View {
         VStack {
-            Spacer()
-            
-            HStack {
-                Image(systemName: "person.fill")
-                    .foregroundColor(.secondary)
-                
-                TextField(text: $email) {
-                    Label("이메일", systemImage: "lock.fill")
-                }
-            }
-            .modifier(LoginTextFieldModifier())
-            .padding(.bottom)
-            
-            HStack {
-                ZStack {
-                    HStack {
-                        Image(systemName: "lock.fill")
-                            .foregroundColor(.secondary)
-                        if isPasswordHidden {
-                            TextField("비밀번호", text: $password)
-                        } else {
-                            SecureField("비밀번호", text: $password)
-                        }
-                    }
-                    .modifier(LoginTextFieldModifier())
-                }
-                .overlay(alignment: .topTrailing) {
-                    Button(action: {
-                        isPasswordHidden.toggle()
-                    }, label: {
-                        Image(systemName: isPasswordHidden ? "eye.fill" : "eye.slash.fill")
-                            .foregroundColor(.secondary)
-                    })
-                    .offset(x: -20, y: 20)
-                }
-            }
-            
-            Text(loginMessage)
-                .foregroundColor(.secondary)
-            
-            Spacer()
-            
-            Button(action: {
-                Task {
-                    isLoading.toggle()
-                    let loginCode = await authStores.signIn(email: email, password: password)
-                    if loginCode == .success {
-                        isLogin.toggle()
-                    }
-                    isLoading.toggle()
-                    loginMessage = authStores.getErrorMessage(loginCode: loginCode)
-                }
-            }, label: {
-                Text("로그인")
-                    .padding()
-            })
-            .modifier(LoginButton())
-            
-            Button(action: {
-                isSingUp.toggle()
-            }, label: {
-                Text("회원가입")
-                    .padding()
-            })
-            .modifier(LoginButton())
-            
-            Spacer()
-            
             Button {
                 handleSignInButton()
             } label: {
@@ -103,6 +33,7 @@ struct LoginView: View {
                     Spacer()
                     Image("googleButton")
                         .resizable()
+                        .scaledToFit()
                         .foregroundColor(.white)
                         .frame(width: 30, height: 30)
                     Text("Google로 계속하기")
@@ -110,28 +41,70 @@ struct LoginView: View {
                         .bold()
                     Spacer()
                 }
-                .frame(width: UIScreen.screenWidth - 100, height: 44)
-                .background(.white)
-                .overlay {
-                    Capsule()
-                        .stroke(Color.gray, lineWidth: 1)
-                }
+                .frame(width: UIScreen.screenWidth - 75, height: 44)
+                .border(.gray)
             }
             
-            // FIXME: - Custom Apple Login Button 작업 필요
             Button {
                 appleLogin()
             } label: {
-                Text("애플로 로그인")
+                HStack(alignment: .center) {
+                    Spacer()
+                    Image("appleLoginButton")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.white)
+                        .frame(width: 50, height: 44)
+                    Text("Apple로 계속하기")
+                        .foregroundColor(.black)
+                        .bold()
+                    Spacer()
+                }
+                .frame(width: UIScreen.screenWidth - 75, height: 44)
+                .border(.gray)
             }
         }
-        .padding()
-        
-        .fullScreenCover(isPresented: $isSingUp) {
-            SignUpView(isSignUp: $isSingUp)
-                .environmentObject(authStores)
+        .background(.white)
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Text("나를 찾아주는 100개의 질문에 답을 해보세요.")
+                    .font(.title2)
+                    .bold()
+                
+                Spacer()
+                
+                socialLoginButton
+                
+                Divider()
+                    .padding(.vertical)
+                
+                HStack {
+                    NavigationLink(destination: EmailLoginView(isLogin: $isLogin, isLoading: $isLoading)) {
+                        Text("이메일로 로그인")
+                    }
+                    
+                    NavigationLink(destination: SignUpView()) {
+                        Text("이메일로 회원가입")
+                    }
+                }
+                
+                .foregroundColor(.black)
+                
+                Spacer()
+            }
         }
-        
+        .navigationDestination(isPresented: $isEmailLogin) {
+            EmailLoginView(isLogin: $isLogin, isLoading: $isLoading)
+        }
+        .navigationDestination(isPresented: $isSingUp) {
+            SignUpView()
+        }
+
         .onAppear {
             authStores.startListeners()
         }
