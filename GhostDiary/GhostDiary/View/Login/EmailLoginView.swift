943de3b8cb894080e935284f4410bb29a25f3218
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-25 17:28:25 +0900

    [Add] 이메일로 로그인을 하는 뷰 생성

---
+++ GhostDiary/GhostDiary/View/Login/EmailLoginView.swift
@@ -0,0 +1,21 @@
+//
+//  EmailLoginView.swift
+//  GhostDiary
+//
+//  Created by 이학진 on 2023/01/25.
+//
+
+import SwiftUI
+
+// MARK: - Email로 로그인을 하는 뷰
+struct EmailLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmailLoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmailLoginView()
+    }
+}
