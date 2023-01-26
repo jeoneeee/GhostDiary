bad30cce1c70558f8b7304e23f4536dab2b2680f
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2023-01-26 19:48:54 +0900

    [Add] 타임라인탭의 카테고리를 열거형으로 변경

---
+++ GhostDiary/GhostDiary/Model/TimeLineCategory.swift
@@ -0,0 +1,14 @@
+//
+//  TimeLineCategory.swift
+//  GhostDiary
+//
+//  Created by 이학진 on 2023/01/26.
+//
+
+import Foundation
+
+//MARK: - TimeLine탭의 2개의 카테고리를 표현하는 열거형
+enum TimeLineCategory {
+    case calendar
+    case list
+}
